a7e0d0f345ced77077eff5a20c3404cec6826efd
Fernanda9421 <fer.candrade21@gmail.com>
2022-08-29 06:40:36 -0300

    feat: cria página para renderizar a calculadora

---
+++ src/pages/Calculator/Calculator.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../components/Header/Header';
+
+function Calculator() {
+  return (
+    <div>
+      <Header />
+      <h1>Calculadora</h1>
+    </div>
+  )
+}
+
+export default Calculator;
