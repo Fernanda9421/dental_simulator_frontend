19e5ae2a217a03fc774675271105a131527a43c9
Fernanda9421 <fer.candrade21@gmail.com>
2022-08-30 17:59:33 -0300

    feat: cria e estiliza botão de gerar registros

---
+++ src/components/ButtonReport/ButtonReport.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './buttonReport.css';
+
+function ButtonReport() {
+  return (
+    <button className='button-report'>
+      Gerar relatório
+    </button>
+  );
+}
+
+export default ButtonReport;
