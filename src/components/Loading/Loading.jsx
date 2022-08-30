15bfe3a9e3fd893fa9809556d5802e7a5052161c
Fernanda9421 <fer.candrade21@gmail.com>
2022-08-30 12:57:38 -0300

    feat: cria componente que renderiza o loading da página

---
+++ src/components/Loading/Loading.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { CircleLoader } from "react-spinners";
+
+function Loading() {
+  const override = {
+    display: "block",
+    position: "absolute",
+    top: "50%",
+    marginTop: "-25px",
+    left: "50%",
+    marginLeft: "-100px",
+    borderColor: "purple",
+  };
+
+  return (
+    <div className="sweet-loading">
+      <CircleLoader
+        cssOverride={override}
+        size={150}
+        color={"#123abc"}
+        speedMultiplier={1.5}
+      />
+    </div>
+  );
+}
+
+export default Loading;
