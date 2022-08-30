5ad5e4fee02d029dc8d2e78fd4bed28f5d17d6ed
Fernanda9421 <fer.candrade21@gmail.com>
2022-08-30 12:58:40 -0300

    feat: renderiza componente Loading

--- src/components/ClientCards/ClientCards.jsx
+++ src/components/ClientCards/ClientCards.jsx
@@ -1,19 +1,22 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import DentalSimulatorContext from "../../context/DentalSimulatorContext";
 import { convertDueDate } from "../../utils/convertDueDate";
 import { requestData } from "../../services/requests";
+import Loading from "../Loading/Loading";
 
 import './clientCards.css';
 
 function ClientCards() {
   const { payments, setPayments } = useContext(DentalSimulatorContext);
+  const [isLoading, setIsLoading] = useState(true);
 
   const getPayments = async () => {
     try {
-       const endpoint = '/payment';
+      const endpoint = '/payment';
       const paymentsApi = await requestData(endpoint);
       
       setPayments(paymentsApi);
+      setIsLoading(false);
     } catch (error) {
       console.log(error);
     }
@@ -24,29 +27,37 @@ function ClientCards() {
   }, []);
 
   return (
-    <div className='payment-cards-container'>
-      <h1 className='title'>Lista de clientes</h1>
-      <div className='payment-cards-div'>
-        {
-          payments.map(({ id, attendances, installmentValue, dueDate }) => (
-            <div key={ id } className='payment-card'>
-              <p className='client-name'>
-                { attendances.clients.fullName }
-              </p>
-              <p className='treatment-name'>
-                <strong>Tratamento:</strong> { attendances.treatments.name }
-              </p>
-              <p className='installment-value'>
-                <strong>Valor da parcela:</strong> R$ { installmentValue }
-              </p>
-              <p className='due-date'>
-                <strong>Vencimento:</strong> { convertDueDate(dueDate) }
-              </p>
+    <>
+      {
+        isLoading
+          ? <Loading />
+          : (
+            <div className='payment-cards-container'>
+              <h1 className='title'>Lista de clientes</h1>
+              <div className='payment-cards-div'>
+                {
+                  payments.map(({ id, attendances, installmentValue, dueDate }) => (
+                    <div key={ id } className='payment-card'>
+                      <p className='client-name'>
+                        { attendances.clients.fullName }
+                      </p>
+                      <p className='treatment-name'>
+                        <strong>Tratamento:</strong> { attendances.treatments.name }
+                      </p>
+                      <p className='installment-value'>
+                        <strong>Valor da parcela:</strong> R$ { installmentValue }
+                      </p>
+                      <p className='due-date'>
+                        <strong>Vencimento:</strong> { convertDueDate(dueDate) }
+                      </p>
+                    </div>
+                  ))
+                }
+              </div>
             </div>
-          ))
-        }
-      </div>
-    </div>
+          )
+      }
+    </>
   );
 }
 
