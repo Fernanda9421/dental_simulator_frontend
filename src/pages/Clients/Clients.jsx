c1560b804b90091b893d19384aa986093bf752d4
Fernanda9421 <fer.candrade21@gmail.com>
2022-08-31 16:37:23 -0300

    fix: atualiza renderização de páginas

---
+++ src/pages/Clients/Clients.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ClientCards from "../../components/ClientCards/ClientCards";
+import Header from "../../components/Header/Header";
+
+function Clients() {
+  return (
+    <div>
+      <Header />
+      <ClientCards />
+    </div>
+  );
+}
+
+export default Clients;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import ClientCards from "../../components/ClientCards/ClientCards";
-import Header from "../../components/Header/Header";
+import React from 'react'
+import Header from '../../components/Header/Header';
+import Report from '../../components/Report/Report';
 
 function Home() {
   return (
     <div>
       <Header />
-      <ClientCards />
+      <Report />
     </div>
-  );
+  )
 }
 
 export default Home;
