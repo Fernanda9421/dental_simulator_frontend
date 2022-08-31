432df50af5bc23eff03af3fe021ee57fdb769a5f
Fernanda9421 <fer.candrade21@gmail.com>
2022-08-31 16:38:17 -0300

    feat: cria e estiliza botão que filtra pagamentos por data

--- src/components/ButtonReport/ButtonReport.jsx
+++ src/components/ButtonReport/ButtonReport.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
+import DentalSimulatorContext from "../../context/DentalSimulatorContext";
+import { requestPost } from "../../services/requests";
 
 import './buttonReport.css';
 
 function ButtonReport() {
+  const {
+    startDate, endDate, setPayments,
+  } = useContext(DentalSimulatorContext);
+
+  const getPayments = async () => {
+    try {
+      const endpoint = '/payment';
+      const payments = await requestPost(endpoint, { startDate, endDate });
+
+      setPayments(payments);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <button className='button-report'>
+    <button
+      className='button-report'
+      type="button"
+      onClick={ () => getPayments() }
+    >
       Gerar relatório
     </button>
   );
