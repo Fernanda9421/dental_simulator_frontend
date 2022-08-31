f1eb6700aca4ed7b9c88a731dfc27cb88dd392eb
Fernanda9421 <fer.candrade21@gmail.com>
2022-08-31 16:39:37 -0300

    fix: atualiza componente que renderiza os dados do relatório

--- src/components/Report/Report.jsx
+++ src/components/Report/Report.jsx
@@ -1,16 +1,34 @@
-import React from "react";
-import ButtonReport from "../ButtonReport/ButtonReport";
-import DateSelected from "../DateSelected/DateSelected";
+import React, { useContext } from 'react';
+import ButtonReport from '../ButtonReport/ButtonReport';
+import DateSelected from '../DateSelected/DateSelected';
+import TableReport from '../TableReport/TableReport';
+import { numberOfResults, totalValue } from '../../utils/utils';
 
 import './report.css';
+import DentalSimulatorContext from '../../context/DentalSimulatorContext';
 
 function Report() {
+  const { payments } = useContext(DentalSimulatorContext);
+
   return (
     <div className='report-container'>
       <h1 className='title'>Gerar relatório</h1>
       <div className='report-box'>
         <DateSelected />
         <ButtonReport />
+      </div>
+      <div className='result'>
+        <div className='total-box'>
+          <span className='span-result'>Resultados:</span>
+          <span className='result-value'>{ numberOfResults(payments) }</span>
+        </div>
+        <div className='total-box'>
+          <span className='span-result'>Total à receber:</span>
+          <span className='result-value'>R$ { totalValue(payments) }</span>
+        </div>
+      </div>
+      <div>
+        <TableReport />
       </div>
     </div>
   );
