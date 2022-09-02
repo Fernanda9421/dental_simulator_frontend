b7323680e85dc67c7c711f1f51a439842aa2977b
Fernanda9421 <fer.candrade21@gmail.com>
2022-09-02 07:15:47 -0300

    feat: cria tabela que renderiza as consultas retornadas da API

---
+++ src/components/TableAppointment/TableAppointment.jsx
@@ -0,0 +1,34 @@
+import React, { useContext } from "react";
+import DentalSimulatorContext from "../../context/DentalSimulatorContext";
+import { convertDate } from "../../utils/utils";
+
+function TableAppointment() {
+  const { appointments } = useContext(DentalSimulatorContext);
+
+  return (
+    <div className="table-container">
+      <table id="table">
+        <thead>
+          <tr>
+            <th>Cliente</th>
+            <th>Data do atendimento</th>
+            <th>Tratamento</th>
+            <th>Valor total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {appointments.map(({ id, attendanceDate, treatments, clients }) => (
+            <tr key={id}>
+              <td>{clients.fullName}</td>
+              <td>{convertDate(attendanceDate)}</td>
+              <td>{treatments.name}</td>
+              <td>{treatments.totalPrice}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default TableAppointment;
