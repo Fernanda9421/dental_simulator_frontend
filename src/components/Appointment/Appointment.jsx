0c4093b7245098b333d2c15124371332703c19b2
Fernanda9421 <fer.candrade21@gmail.com>
2022-09-02 07:17:14 -0300

    feat: cria componente que renderiza tabela de consultas

---
+++ src/components/Appointment/Appointment.jsx
@@ -0,0 +1,45 @@
+import React, { useContext, useState, useEffect } from 'react';
+import DentalSimulatorContext from '../../context/DentalSimulatorContext';
+import { requestData } from '../../services/requests';
+import Loading from '../Loading/Loading';
+import TableAppointment from '../TableAppointment/TableAppointment';
+
+function Appointment() {
+  const { setAppointments } = useContext(DentalSimulatorContext);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getAppointments = async () => {
+    try {
+      const endpoint = "/appointment";
+      const allAppointments = await requestData(endpoint);
+
+      setAppointments(allAppointments);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAppointments();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      {
+        isLoading ? (
+          <Loading />
+        ) : (
+          <div className="appointment-container">
+            <div>
+              <TableAppointment />
+            </div>
+          </div>
+        )
+      }
+    </>
+  );
+}
+
+export default Appointment;
---
+++ src/pages/Appointments/Appointments.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+import Appointment from "../../components/Appointment/Appointment";
+
+function Appointments() {
+  return (
+    <div>
+      <Header />
+      <Appointment />
+    </div>
+  );
+}
+
+export default Appointments;
