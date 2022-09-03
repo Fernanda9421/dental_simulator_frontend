6fd9484c858cf261767f19081bdd633a882fa41f
Fernanda9421 <fer.candrade21@gmail.com>
2022-09-03 14:39:58 -0300

    feat: cria página que renderiza formulário de cadastrar consulta

---
+++ src/pages/NewAppointment/NewAppointment.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppointmentRegistration from '../../components/AppointmentRegistration/AppointmentRegistration';
+import Header from '../../components/Header/Header';
+
+function NewAppointment() {
+  return (
+    <div>
+      <Header />
+      <AppointmentRegistration />
+    </div>
+  );
+}
+
+export default NewAppointment;
