baa18ff60c234a1d763eecfc7440f2a785d2c23c
Fernanda9421 <fer.candrade21@gmail.com>
2022-09-03 14:41:59 -0300

    feat: cria componente que renderiza input com data de atendimento

---
+++ src/components/AppointmentRegistration/AttendanceDate.jsx
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react';
+import DatePicker from "react-datepicker";
+import DentalSimulatorContext from '../../context/DentalSimulatorContext';
+
+function AttendanceDate() {
+  const { newAppointment, setNewAppointment } = useContext(DentalSimulatorContext);
+
+  return (
+    <div className='input-container'>
+      <label>Data do atendimento</label>
+      <DatePicker
+        dateFormat="dd/MM/yyyy"
+        className='input-date'
+        selected={newAppointment.attendanceDate}
+        onChange={ (date) => {
+          setNewAppointment({ ...newAppointment, attendanceDate: date })
+        }}
+      />
+    </div>
+  );
+}
+
+export default AttendanceDate;
