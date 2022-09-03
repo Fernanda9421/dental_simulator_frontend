58fd0adc64ca5397b9184faff34d5db5ce5beaba
Fernanda9421 <fer.candrade21@gmail.com>
2022-09-03 14:43:34 -0300

    feat: cria rota /new/appointment

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Appointments from './pages/Appointments/Appointments';
+import NewAppointment from './pages/NewAppointment/NewAppointment';
 
 function DentalSimulatorRoutes() {
   return (
     <Routes>
       <Route path='/' element={ <Home /> } />
       <Route path='/appointments' element={ <Appointments /> } />
+      <Route path='/new/appointment' element={<NewAppointment />} />
     </Routes>
   );
 }
