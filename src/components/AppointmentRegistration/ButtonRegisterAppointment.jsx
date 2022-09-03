316c970d9b9ed59f575dc49ba184f136a5ddc03f
Fernanda9421 <fer.candrade21@gmail.com>
2022-09-03 14:42:20 -0300

    feat: cria botão de registrar nova consulta

---
+++ src/components/AppointmentRegistration/ButtonRegisterAppointment.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function ButtonRegisterAppointment() {
+  return (
+    <div className='button-register'>
+      <button
+        className='button-report register'
+        type="button"
+      >
+        Cadastrar
+      </button>
+    </div>
+  );
+}
+
+export default ButtonRegisterAppointment;
