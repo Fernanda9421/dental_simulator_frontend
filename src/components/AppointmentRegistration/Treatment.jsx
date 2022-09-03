58b69320fbd4073d2aa1a23e52b65bd6f05ca8db
Fernanda9421 <fer.candrade21@gmail.com>
2022-09-03 14:40:30 -0300

    feat: cria componente que busca tratamentos cadastrados no banco

---
+++ src/components/AppointmentRegistration/Treatment.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react';
+import { useContext } from 'react';
+import Select from 'react-select';
+import DentalSimulatorContext from '../../context/DentalSimulatorContext';
+import { requestData } from '../../services/requests';
+
+function Treatment() {
+  const {
+    treatments, setTreatments, newAppointment, setNewAppointment,
+  } = useContext(DentalSimulatorContext);
+
+  const getTreatments = async () => {
+    try {
+      const endpoint = "/treatment";
+      const allTreatments = await requestData(endpoint);
+
+      setTreatments(allTreatments);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getTreatments();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const options = () => {
+    const newTreatments = [];
+
+    treatments.map(({ name }) => (
+      newTreatments.push({ value: name, label: name })
+    ));
+    return newTreatments;
+  }
+
+  return (
+    <div className='input-container'>
+      <label>Tratamento</label>
+      <Select
+        options={ options() }
+        className='select-treatment input-appointments'
+        onChange={ ({ value }) =>  (
+          setNewAppointment({ ...newAppointment, treatment: value })
+        )}
+      />
+    </div>
+  );
+}
+
+export default Treatment;
