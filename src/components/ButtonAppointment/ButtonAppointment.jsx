1fd2d76d99a7f98b3d80288abc671c343a83e095
Fernanda9421 <fer.candrade21@gmail.com>
2022-09-03 14:38:14 -0300

    feat: cria botão que redireciona para página de cadastrar nova consulta

---
+++ src/components/ButtonAppointment/ButtonAppointment.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import './buttonAppointment.css';
+
+function ButtonAppointment() {
+  const navigate = useNavigate();
+
+  const navigateToNewAppointment = () => navigate('/new/appointment');
+
+  return (
+    <div className='button-appointment-box'>
+      <button
+        className='button-report'
+        type="button"
+        onClick={ () => navigateToNewAppointment() }
+      >
+        Cadastrar consulta
+      </button>
+    </div>
+  );
+}
+
+export default ButtonAppointment;
