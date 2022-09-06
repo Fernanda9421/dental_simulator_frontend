756df462584141187523b6ab4e2e698b0074035a
Fernanda9421 <fer.candrade21@gmail.com>
2022-09-06 16:31:19 -0300

    feat: cria funcionalidade em botão de cadastrar nova consulta no banco de dados

--- src/components/AppointmentRegistration/ButtonRegisterAppointment.jsx
+++ src/components/AppointmentRegistration/ButtonRegisterAppointment.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import DentalSimulatorContext from '../../context/DentalSimulatorContext';
+import { requestPost } from '../../services/requests';
+import { numberOfInstallments } from '../../utils/utils';
 
 function ButtonRegisterAppointment() {
+  const { newAppointment, setNewAppointment } = useContext(DentalSimulatorContext);
+  const navigate = useNavigate();
+
+  const navigateToHome = () => navigate('/');
+
+  const createAppointment = async () => {
+    try {
+      const { attendanceDate, clientName, treatment, dueDate, installments } = newAppointment;
+      const installmentsArray = numberOfInstallments(installments);
+      const endpoint = '/appointment';
+
+      await requestPost(endpoint, {
+        attendanceDate, clientName, treatment, dueDate, installments: installmentsArray,
+      });
+
+      navigateToHome();
+      setNewAppointment({
+        attendanceDate: new Date(),
+        clientName: '',
+        treatment: '',
+        installments: 1,
+        dueDate: new Date(),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className='button-register'>
       <button
         className='button-report register'
-        type="button"
+        type='button'
+        onClick={ () => createAppointment() }
       >
         Cadastrar
       </button>
