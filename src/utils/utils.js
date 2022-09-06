105dab010a9e3e3301968c6eaa8ecb905a39352b
Fernanda9421 <fer.candrade21@gmail.com>
2022-09-06 16:30:08 -0300

    feat: cria função auxiliar que retorna o numero de parcelas em um array

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -10,3 +10,13 @@ export const totalValue = (results) => {
   const values = results.map((result) => Number(result.installmentValue));
   return values.reduce((acc, crr) => acc + crr, 0).toFixed(2);
 };
+
+export const numberOfInstallments = (installments) => {
+  const installmentsArray = [];
+
+  for (let index = 1; index <= installments; index += 1) {
+    installmentsArray.push(index);
+  }
+
+  return installmentsArray;
+};
